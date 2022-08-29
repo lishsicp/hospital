d7661584a16ee6293c7d4db4ea35d915e07a81b9
Yaroslav Lobur <lobur13@gmail.com>
2022-08-29 20:22:14 +0300

    Documentation for GenericDao

--- src/main/java/com/yaroslav/lobur/model/dao/GenericDao.java
+++ src/main/java/com/yaroslav/lobur/model/dao/GenericDao.java
@@ -12,12 +12,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Generic DAO (Data Access Object) with most common methods.
+ * @param <T> a type variable (Must implement <code>Entity</code> interface)
+ */
 public abstract class GenericDao<T extends Entity>  {
 
     private static final Logger logger = LoggerFactory.getLogger(GenericDao.class);
 
     private int noOfRecords;
 
+    /**
+     * Generic method used to get all objects of a particular type. This
+     * is the same as lookup up all rows in a table.
+     *
+     * @param con a connection
+     * @param sql SQL query <code>String</code>
+     * @return List of populated entities
+     */
     protected List<T> findAll(Connection con, String sql) {
         List<T> list = new ArrayList<>();
         PreparedStatement ps = null;
@@ -41,10 +53,23 @@ public abstract class GenericDao<T extends Entity>  {
         }
     }
 
+    /**
+     * @return Number of records in the table for given SQL query
+     * (must add <tt>SQL_CALC_FOUND_ROWS</tt> to query string and execute
+     * query with <code>SELECT FOUND_ROWS()</code> to assign value).
+     */
     protected int getNumberOfRecords() {
         return noOfRecords;
     }
 
+    /**
+     * Generic method to get a List of entities based on values.
+     * @param con a connection
+     * @param sql SQL query <code>String</code>
+     * @param values varargs for values to be inserted into SQL Query
+     * @param <V> can be null, int, long, String or Date
+     * @return List of populated entities based on values
+     */
     @SafeVarargs
     protected final <V> List<T> findEntities(Connection con, String sql, V... values) {
         List<T> list = new ArrayList<>();
@@ -71,9 +96,16 @@ public abstract class GenericDao<T extends Entity>  {
         }
     }
 
+    /**
+     * @param ps An object that represents a precompiled SQL statement.
+     * @param values varargs for values to be inserted into SQL Query
+     * @param <V> can be null, int, long, String or Date
+     * @return filled Result Set based of values
+     * @throws SQLException if a database access error occurs or this method is called on a closed connection
+     */
     @SafeVarargs
     private <V> ResultSet setResultSetValues(PreparedStatement ps, V... values) throws SQLException {
-        ResultSet rs = null;
+        ResultSet rs;
         int pos = 1;
         for (V value : values) {
             if (value == null) {
@@ -93,6 +125,18 @@ public abstract class GenericDao<T extends Entity>  {
         return rs;
     }
 
+    /**
+     * Generic method to get an object based on values. An
+     * EntityNotFoundException Runtime Exception is thrown if
+     * nothing is found.
+     * @param con a connection
+     * @param sql SQL query <code>String</code>
+     * @param values varargs for values to be inserted into SQL Query
+     * @param <V> can be null, int, long, String or Date
+     * @return Single Entity from table based on values
+     * @throws EntityNotFoundException if no entity was found
+     * @throws UnknownSqlException wraps SQLException
+     */
     @SafeVarargs
     protected final <V> T findEntity(Connection con, String sql, V... values) {
         T entity;
@@ -113,6 +157,15 @@ public abstract class GenericDao<T extends Entity>  {
         }
     }
 
+    /**
+     * Generic method to save an Entity.
+     * @param connection a connection
+     * @param sql SQL query <code>String</code>
+     * @param item Entity to be inserted
+     * @return the id of inserted Entity
+     * @throws InputErrorsMessagesException map of error messages
+     * @throws UnknownSqlException wraps SQLException
+     */
     protected long insertEntity(Connection connection, String sql, T item) {
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -133,6 +186,13 @@ public abstract class GenericDao<T extends Entity>  {
         }
     }
 
+    /**
+     * Generic method that remove entity
+     * @param connection a connection
+     * @param sql SQL query <code>String</code>
+     * @throws InputErrorsMessagesException map of error messages
+     * @throws UnknownSqlException wraps SQLException
+     */
     protected void deleteEntity(Connection connection, String sql) {
         PreparedStatement ps = null;
         try {
@@ -147,6 +207,14 @@ public abstract class GenericDao<T extends Entity>  {
         }
     }
 
+    /**
+     * Generic method to update Entity.
+     * @param connection a connection
+     * @param sql SQL query <code>String</code>
+     * @param item Entity to be updated
+     * @throws InputErrorsMessagesException map of error messages
+     * @throws UnknownSqlException wraps SQLException
+     */
     protected void updateEntity(Connection connection, String sql, T item) {
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             mapFromEntity(ps, item);
@@ -157,10 +225,27 @@ public abstract class GenericDao<T extends Entity>  {
         }
     }
 
+    /**
+     * Abstract method, that extracts entity fields from the current row of the provided ResultSet.
+     * @param rs ResultSet
+     * @return Entity with values set from ResultSet
+     * @throws SQLException if a database access error occurs or this method is called on a closed connection
+     */
     protected abstract T mapToEntity(ResultSet rs) throws SQLException;
 
+    /**
+     * Abstract method, which is used for setting up PreparedStatement parameters, based on entity's fields values
+     * @param statement PreparedStatement
+     * @param entity Entity that PreparedStatement is filled from.
+     * @throws SQLException if a database access error occurs or this method is called on a closed connection
+     */
     protected abstract void mapFromEntity(PreparedStatement statement, T entity) throws SQLException;
 
+    /**
+     * Closes ResultSet and Statement
+     * @param rs ResultSet
+     * @param statement Statement
+     */
     protected void closeResultSetAndPreparedStatement(ResultSet rs, Statement statement) {
         if (rs != null) {
             try {
